83cbb83656feb0aa21b1793a7d1501755b49f7d6
swk <shiwk@tju.edu.cn>
2019-11-30 20:52:46 +0800

    Fix proposer authority bug.

--- contract/AElf.Contracts.Genesis/BasicContractZero.cs
+++ contract/AElf.Contracts.Genesis/BasicContractZero.cs
@@ -77,7 +77,7 @@ namespace AElf.Contracts.Genesis
         public override Address DeploySystemSmartContract(SystemContractDeploymentInput input)
         {
             RequireSenderAuthority(State.GenesisOwner?.Value);
-            ValidateProposerAuthority(Context.Origin);
+            ValidateProposerAuthority(Context.Origin, true);
             var name = input.Name;
             var category = input.Category;
             var code = input.Code.ToByteArray();
@@ -159,7 +159,7 @@ namespace AElf.Contracts.Genesis
         public override Hash ProposeNewContract(ContractDeploymentInput input)
         {
             RequireSenderAuthority();
-            ValidateProposerAuthority(Context.Origin);
+            ValidateProposerAuthority(Context.Origin, false);
             var proposedContractInputHash = CalculateHashFromInput(input);
             Assert(State.ContractProposingInfoMap[proposedContractInputHash] == null, "Already proposed.");
             State.ContractProposingInfoMap.Set(proposedContractInputHash,
@@ -205,7 +205,6 @@ namespace AElf.Contracts.Genesis
         public override Hash ProposeUpdateContract(ContractUpdateInput input)
         {
             RequireSenderAuthority();
-            ValidateProposerAuthority(Context.Origin);
             var proposedContractInputHash = CalculateHashFromInput(input);
             Assert(State.ContractProposingInfoMap[proposedContractInputHash] == null, "Already proposed.");
             State.ContractProposingInfoMap.Set(proposedContractInputHash,
@@ -218,7 +217,8 @@ namespace AElf.Contracts.Genesis
             var info = State.ContractInfos[contractAddress];
             Assert(info != null, "Contract does not exist.");
             AssertAuthorityByContractInfo(info);
-            
+            ValidateProposerAuthority(Context.Origin, info.IsSystemContract);
+
             // Create proposal for deployment
             RequireParliamentAuthAddressSet();
             State.ParliamentAuthContract.CreateProposal.Send(new CreateProposalInput
@@ -243,7 +243,7 @@ namespace AElf.Contracts.Genesis
         public override Address DeploySmartContract(ContractDeploymentInput input)
         {
             RequireSenderAuthority();
-            ValidateProposerAuthority(Context.Origin);
+            ValidateProposerAuthority(Context.Origin, false);
             var inputHash = CalculateHashFromInput(input);
             RemoveContractProposingInfo(inputHash);
             var address = PrivateDeploySystemSmartContract(null, input.Category, input.Code.ToByteArray(), false);
@@ -267,7 +267,7 @@ namespace AElf.Contracts.Genesis
             }
             
             AssertAuthorityByContractInfo(info);
-            ValidateProposerAuthority(Context.Origin);
+            ValidateProposerAuthority(Context.Origin, info.IsSystemContract);
 
             var inputHash = CalculateHashFromInput(input);
             RemoveContractProposingInfo(inputHash);
@@ -400,15 +400,16 @@ namespace AElf.Contracts.Genesis
             return Hash.FromMessage(input);
         }
 
-        private void ValidateProposerAuthority(Address proposer)
+        private void ValidateProposerAuthority(Address proposer, bool parliamentMemberNeeded)
         {
             var isGenesisOwnerAuthorityRequired = State.ContractDeploymentAuthorityRequired.Value;
             if (!isGenesisOwnerAuthorityRequired)
                 return;
             
             RequireParliamentAuthAddressSet();
-            var proposerAuthorityValidated = State.ParliamentAuthContract.ValidateAddressInProposerWhiteList
-                .Call(proposer).Value;
+            var proposerAuthorityValidated = parliamentMemberNeeded
+                ? State.ParliamentAuthContract.ValidateAddressIsParliamentMember.Call(proposer).Value
+                : State.ParliamentAuthContract.ValidateProposerAuthority.Call(proposer).Value;
             Assert(proposerAuthorityValidated, "Proposer authority validation failed.");
         }
 
